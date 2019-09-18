1860bed53636f8b5d63981633a8be42437a76630
Jason Lengstorf <jason@lengstorf.com>
2019-09-18 15:48:19 -0700

    wip: add simple commands
    This currently won’t work. It’s blocked by instafluff/ComfyJS#4

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const comfy = require('comfy.js');
+
+const commands = {
+  blitzed: (user, message, flags, extra) => {
+    console.log({ user, message, flags, extra });
+    comfy.Say('KAPOW YOU GOT BLITZED! MorphinTime', extra.channel);
+  },
+  echo: (user, message, { broadcaster, mod }, { channel }) => {
+    if (!broadcaster && !mod) {
+      comfy.Say(
+        `You’re not my real dad, @${user}. I don’t have to listen to you.`,
+        channel,
+      );
+      return;
+    }
+
+    comfy.Say(message, channel);
+  },
+};
+
+comfy.onCommand = (user, command, message, flags, extra) => {
+  if (Object.keys(commands).includes(command)) {
+    commands[command](user, message, flags, extra);
+  }
+};
+
+comfy.Init(
+  process.env.TWITCHUSER,
+  process.env.OAUTH,
+  ['jlengstorf', 'chrisbiscardi'],
+  true,
+);
