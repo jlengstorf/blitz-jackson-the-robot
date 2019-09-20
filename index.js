30983f9361f799449dce960c89b334f20036ab2b
Jason Lengstorf <jason@lengstorf.com>
2019-09-20 16:34:40 -0700

    fix: Chris command + minor refactor (#4)

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 require('dotenv').config();
 const comfy = require('comfy.js');
 
+const naughtyNaughty = (user, channel) =>
+  comfy.Say(
+    `You’re not my supervisor, @${user}. I don’t have to listen to you.`,
+    channel,
+  );
+
 const commands = {
   blitzed: (user, message, flags, extra) => {
     console.log({ user, message, flags, extra });
     comfy.Say('KAPOW YOU GOT BLITZED! MorphinTime', extra.channel);
   },
-  chris: (user, _,_, {channel}) => {
-    if(user !== 'chrisbiscardi') {
-    comfy.Say('LISTEN TO CHRIS! Chris is always right jlengsBeardy')
+  chris: (user, _message, _flags, { channel }) => {
+    if (user !== 'chrisbiscardi') {
+      comfy.Say('LISTEN TO CHRIS! Chris is always right jlengsBeardy', channel);
     } else {
-      comfy.Say('jlengsHolyBucket Yes, you’re Chris. jlengsHolyBucket', channel)
+      comfy.Say(
+        'jlengsHolyBucket Yes, you’re Chris. jlengsHolyBucket',
+        channel,
+      );
     }
   },
   echo: (user, message, { broadcaster, mod }, { channel }) => {
     if (!broadcaster && !mod) {
-      comfy.Say(
-        `You’re not my real dad, @${user}. I don’t have to listen to you.`,
-        channel,
-      );
+      naughtyNaughty(user, channel);
       return;
     }
 
