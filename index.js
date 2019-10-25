5e208a622e291b30060c7c4b6b3637e5ae6a2496
Jason Lengstorf <jason@lengstorf.com>
2019-10-25 12:55:52 -0700

    feat: add Fauna support for custom commands

--- index.js
+++ index.js
@@ -1,5 +1,111 @@
 require('dotenv').config();
 const comfy = require('comfy.js');
+const axios = require('axios');
+
+let dbCommands = [];
+
+const sendQuery = async (query, variables = {}) => {
+  const { data, errors } = await axios
+    .post(
+      'https://graphql.fauna.com/graphql',
+      {
+        query,
+        variables,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+        },
+      },
+    )
+    .catch(error => {
+      console.error(error);
+    });
+
+  if (errors) {
+    console.error(errors);
+  }
+
+  return data.data;
+};
+
+const loadCommands = async () => {
+  const data = await sendQuery(`
+    query {
+      getActiveCommands(isActive: true) {
+        data {
+          _id
+          command
+          message
+        }
+      }
+    }
+  `);
+
+  dbCommands = data.getActiveCommands.data;
+};
+
+const updateOrCreateCommand = async (command, message) => {
+  const currentCommand = dbCommands.find(cmd => cmd.command === command);
+
+  if (currentCommand) {
+    const data = await sendQuery(
+      `
+        mutation ($id: ID! $command: String! $message: String!) {
+          updateCommand(
+            id: $id
+            data: {
+              command: $command
+              message: $message
+              timeout: 60
+              isActive: true
+            }
+          ) {
+            _id
+            command
+            message
+          }
+        }
+      `,
+      {
+        id: currentCommand._id,
+        command: currentCommand.command,
+        message,
+      },
+    );
+
+    // TODO save the updated command to dbCommands
+    const cmdIndex = dbCommands.findIndex(cmd => cmd.command === command);
+    dbCommands[cmdIndex] = data.updateCommand;
+
+    console.log(dbCommands);
+    return;
+  }
+
+  const data = await sendQuery(
+    `
+      mutation ($command: String! $message: String!) {
+        createCommand(data: {
+          command: $command
+          message: $message
+          timeout: 60
+          isActive: true
+        }) {
+          _id
+          command
+          message
+        }
+      }
+    `,
+    {
+      command,
+      message,
+    },
+  );
+
+  dbCommands = dbCommands.concat(data.createCommand);
+  console.log(dbCommands);
+};
 
 const naughtyNaughty = (user, channel) =>
   comfy.Say(
@@ -30,26 +136,51 @@ const commands = {
 
     comfy.Say(message, channel);
   },
-  so: (user, message, {broadcaster, mod}, {channel}) => {
+  so: (user, message, { broadcaster, mod }, { channel }) => {
     // only broadcasters or mods can shoutout
     // don't want the !so command to fight with any existing channel !so commands
     // so should return if not BOTOWNER
-    if((!broadcaster && !mod) || channel !== process.env.BOTOWNER ) {
+    if ((!broadcaster && !mod) || channel !== process.env.BOTOWNER) {
       return;
     }
-    const parts = message.split(" ");
-    const targetChannel = parts.find(part => part.startsWith("@"))
-    if(!targetChannel) {
+    const parts = message.split(' ');
+    const targetChannel = parts.find(part => part.startsWith('@'));
+    if (!targetChannel) {
       return;
     }
-    comfy.Say(`jlengsHolyBucket Please checkout ${targetChannel} at https://twitch.tv/${targetChannel.replace('@','')} jlengsBeardy `)
-  }
+    comfy.Say(
+      `jlengsHolyBucket Please checkout ${targetChannel} at https://twitch.tv/${targetChannel.replace(
+        '@',
+        '',
+      )} jlengsBeardy `,
+    );
+  },
 };
 
 comfy.onCommand = (user, command, message, flags, extra) => {
+  const currentCommand = dbCommands.find(cmd => cmd.command === command);
+  const canEdit = flags.broadcaster || flags.mod;
+
+  if (command === 'commander' && canEdit) {
+    const parts = message.split(' ');
+    const [cmd, ...msgParts] = parts;
+    const msg = msgParts.join(' ');
+
+    updateOrCreateCommand(cmd, msg);
+  }
+
+  if (currentCommand) {
+    console.log(JSON.stringify(flags));
+    comfy.Say(currentCommand.message, extra.channel);
+  }
+
   if (Object.keys(commands).includes(command)) {
     commands[command](user, message, flags, extra);
   }
+};
+
+comfy.onConnected = () => {
+  loadCommands();
 };
 
 comfy.Init(
