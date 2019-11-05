9a575fc44762e331bc8e400567a7ce9f5bd95fb1
Jason Lengstorf <jason@lengstorf.com>
2019-11-04 16:45:39 -0800

    feat: add the pusher

--- actions/text.js
+++ actions/text.js
@@ -2,11 +2,13 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+const pusher = process.env.GITHUB_ACTOR;
+
 client.messages
   .create({
     from: '+13345186489',
     to: process.env.TWILIO_PHONE_NUMBER,
-    body: 'Someone pushed to BlitzBot',
+    body: `${pusher} pushed to BlitzBot`,
   })
   .then(message => console.log(message.sid))
   .done();
