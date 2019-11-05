ca0c57ee2134f7f778044d21ae32b7d369e6b2b2
Jason Lengstorf <jason@lengstorf.com>
2019-11-04 16:10:51 -0800

    chore: custom actions

---
+++ actions/text.js
@@ -0,0 +1,11 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+    from: '+13345186489',
+    to: process.env.TWILIO_PHONE_NUMBER,
+  })
+  .then(message => console.log(message.sid))
+  .done();
