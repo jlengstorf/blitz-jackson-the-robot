38e1c62a3cc6edd87085a0b49ec63d9456481ab4
Jason Lengstorf <jason@lengstorf.com>
2019-11-11 18:33:50 -0800

    fix: add SSL support

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
 const WebSocket = require('ws');
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const http = require('http');
+const https = require('https');
 
 const initializeChatbot = require('./chatbot');
 
@@ -11,7 +13,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client')));
 
-const server = http.createServer(app);
+const server = https.createServer(
+  {
+    cert: fs.readFileSync(process.env.SSL_CERT),
+    key: fs.readFileSync(process.env.SSL_KEY),
+  },
+  app,
+);
+
 const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
 
 initializeChatbot(wss);
@@ -19,27 +28,9 @@ initializeChatbot(wss);
 server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, ws => {
     wss.emit('connection', ws, request);
-    console.log('sent a connection event');
   });
 });
 
 server.listen(8080, () => {
-  console.log('Listening on http://localhost:8080');
+  console.log(`Listening on https://localhost:${server.address().port}`);
 });
-
-// const io = require('./events');
-// io.on('connection', socket => {
-//   socket.on('heyoooo', data => {
-//     console.log(JSON.stringify(data));
-//   });
-
-//   setInterval(() => {
-//     socket.broadcast.emit('toodles', { broadcast: true });
-//   }, 2000);
-
-//   // socket.on('disconnect', () => {
-//   //   socket.removeAllListeners('test');
-//   // });
-
-//   initializeChatbot(socket);
-// });
