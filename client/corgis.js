80d6e59e3bd26ff12c3d7dfab88e0a9d1013c0ef
Jason Lengstorf <jason@lengstorf.com>
2019-11-12 20:26:22 -0800

    feat: better corgi running

--- client/corgis.js
+++ client/corgis.js
@@ -12,23 +12,26 @@ export default () => {
 
   let corgiCoordinates = [];
   for (let i = 0; i < 30; i++) {
+    const duration = Math.round(Math.random() * 3000) + 2000;
     corgiCoordinates.push({
+      duration,
       runDelay: Math.round(Math.random() * 3000),
-      jumpDelay: Math.round(Math.random() * 6000) + 1000,
+      jumpDelay: Math.round(Math.random() * duration),
       top: Math.round(Math.random() * 100),
     });
   }
 
   let corgis = [];
-  const sorted = corgiCoordinates
+  corgiCoordinates
     .sort((a, b) => a.top - b.top)
     .map((coords, i) => ({ ...coords, zIndex: i + 1 }))
     .forEach(coords => {
-      const { runDelay, jumpDelay, top, zIndex } = coords;
+      const { duration, runDelay, jumpDelay, top, zIndex } = coords;
       const el = corgi.cloneNode();
 
       el.style.cssText = `
         animation-delay: ${runDelay}ms, ${jumpDelay}ms;
+        animation-duration: ${duration}ms, 500ms;
         top: ${top}px;
         z-index: ${zIndex};
       `;
