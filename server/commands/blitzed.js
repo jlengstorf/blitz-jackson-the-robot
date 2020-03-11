565d752c7046070374397774eea1e53c06b4c0bc
Jason Lengstorf <jason@lengstorf.com>
2020-03-11 08:14:55 -0700

    feat: commands have moved to Stream Blitz

--- server/commands/blitzed.js
+++
@@ -1,9 +0,0 @@
-module.exports = (_user, _message, _flags, { channel }) => ({
-  name: 'blitzed',
-  message: 'KAPOW YOU GOT BLITZED! MorphinTime',
-  sfx:
-    'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/lwj-sfx/blitzed.mp3',
-  gif:
-    'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_400/v1573512575/lwj-sfx/victory',
-  channel,
-});
--- server/commands/chris.js
+++
@@ -1,8 +0,0 @@
-module.exports = (user, _message, _flags, { channel }) => ({
-  name: 'chris',
-  message:
-    user !== 'chrisbiscardi'
-      ? 'LISTEN TO CHRIS! Chris is always right chrisb24PartyCorgi chrisb24PartyCorgi'
-      : 'jlengsHolyBucket Yes, you’re Chris. jlengsHolyBucket',
-  channel,
-});
